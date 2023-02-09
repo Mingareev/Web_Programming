ce5c82200e100a1f6dc1a52e5c6a896ded8c79bf
Mingareev <81474718+Mingareev@users.noreply.github.com>
2023-02-10 00:47:57 +0500

    Загрузил код 1 лабы

---
+++ 1/index.php
@@ -0,0 +1,26 @@
+<html>
+<head> <title>Веб-прог. Лаб. работа 1 </title> </head>
+<div align="center">
+    <h1>Веб-прог. Мингареев Радмир Адикович<p> Лаб. работа 1</h1><p>
+</div>
+<body>
+<u1>
+        <li><a href="http://a0777012.xsph.ru/1/lab-1-1.php">Лаба-1-1</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-2.php">Лаба-1-2</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-3.php">Лаба-1-3</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-4.php">Лаба-1-4</a></li>
+        <li><a href="http://a0777012.xsph.ru/1/lab-1-5.php">Лаба-1-5 Варианты 3 и 8</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-6.php">Лаба-1-6</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-7.php">Лаба-1-7</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-8.php">Лаба-1-8</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-9.php">Лаба-1-9</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-10.php">Лаба-1-10</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-11.php">Лаба-1-11</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-12.php">Лаба-1-12</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-13.php">Лаба-1-13</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-14.php">Лаба-1-14</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-15.php">Лаба-2-10</a></li>
+		<li><a href="http://a0777012.xsph.ru/1/lab-1-16.php">Лаба-2-11 (2 и 7 задача)</a></li>
+</u1>
+</body>
+</html>
