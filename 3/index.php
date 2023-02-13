587640bd431033c2cc04240865bc49149fc72a2d
Mingareev <81474718+Mingareev@users.noreply.github.com>
2023-02-14 02:09:23 +0500

    Загрузил код 3 лабы

---
+++ 3/index.php
@@ -0,0 +1,15 @@
+
+<HTML>
+<BODY>
+     <p>Мингареев Радмир Адикович</p>
+    <h2> Лабораторная работа № 3 </h2>
+        <li><a href="http://a0777012.xsph.ru/3/lab-3-1.html">Упр. 3-1</a></li>
+        <li><a href="http://a0777012.xsph.ru/3/lab-3-2.php">Упр. 3-2</a></li>
+        <li><a href="http://a0777012.xsph.ru/3/lab-3-3.php">Упр. 3-3</a></li>
+        <li><a href="http://a0777012.xsph.ru/3/lab-3-4.php">Упр. 3-4</a></li>
+        <li><a href="http://a0777012.xsph.ru/3/lab-3-5.php">Упр. 3-5</a></li>
+        <li><a href="http://a0777012.xsph.ru/3/lab-3-6.2.php">Упр. 3-6.2</a></li>
+        <li><a href="http://a0777012.xsph.ru/3/lab-3-6.4.php">Упр. 3-6.4</a></li>
+        <li><a href="http://a0777012.xsph.ru/3/lab-3-6.6.php">Упр. 3-6.6</a></li>
+</BODY> 
+</HTML>
