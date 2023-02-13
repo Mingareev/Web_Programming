e267536338481a405ac16a51f81e62ee697ef93b
Mingareev <81474718+Mingareev@users.noreply.github.com>
2023-02-14 01:43:54 +0500

    Загрузил код 2 лабы

---
+++ 2/index.php
@@ -0,0 +1,16 @@
+<html>
+<head> <title>Веб-прог. Лаб. работа 2 </title> </head>
+<div align="center">
+    <h1>Веб-прог. Мингареев Радмир Адикович<p> Лаб. работа 2</h1><p>
+</div>
+<body>
+<u1>
+    <li><a href="http://a0777012.xsph.ru/2/lab-2-1.php">lab-2-1</li>
+	<li><a href="http://a0777012.xsph.ru/2/lab-2-2.php">lab-2-2</li>
+	<li><a href="http://a0777012.xsph.ru/2/lab-2-3.php">lab-2-3</li>
+	<li><a href="http://a0777012.xsph.ru/2/lab-2-4.php">lab-2-4</li>
+	<li><a href="http://a0777012.xsph.ru/2/lab-2-5.php">lab-2-5</li>
+	<li><a href="http://a0777012.xsph.ru/2/lab-2-6.php">lab-2-6</li>
+</u1>
+</body>
+</html>
